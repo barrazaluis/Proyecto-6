2e79337a85da7cb6433cdd1f2a3b1acdbb622941
barrazaluis <cbarrazaluis@gmail.com>
2026-01-19 20:13:22 -0300

    server.js actualizado

--- server.js
+++ server.js
@@ -20,3 +20,7 @@ app.use("/api/product", productRoutes)
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 )
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API funcionando correctamente' });
+});
