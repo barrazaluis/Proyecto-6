61c8b5f5743a7d01f4fba8ffcee5f6136a79ee4c
barrazaluis <cbarrazaluis@gmail.com>
2026-01-19 21:00:44 -0300

    update.js

--- server.js
+++ server.js
@@ -3,28 +3,33 @@ const cors = require("cors")
 require("dotenv").config()
 
 const connectDB = require("./config/db")
-
 const userRoutes = require("./routes/userRoutes")
 const productRoutes = require("./routes/productRoutes")
+const { swaggerUi, swaggerSpec } = require("./swagger")
 
 const app = express()
 
+// Conexión a DB
 connectDB()
 
+// Middlewares
 app.use(cors())
 app.use(express.json())
 
+// Rutas
 app.use("/api/user", userRoutes)
 app.use("/api/product", productRoutes)
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-)
+// Ruta raíz (para Render / verificación)
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionando correctamente" })
+})
 
-app.get('/', (req, res) => {
-  res.json({ message: 'API funcionando correctamente' });
-});
-
-const { swaggerUi, swaggerSpec } = require("./swagger")
-
+// Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+// Puerto (Render usa process.env.PORT)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
